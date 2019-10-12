4714e2caf54729f082d0ddc24f945b36a7ab6683
Juan Pablo Lescano <juanpablo.lescano@gmail.com>
2019-10-12 14:07:50 -0300

    reservation consumer

---
+++ reservation-consumer/src/main/java/com/example/reservationconsumer/ReservationConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationConsumerApplication.class, args);
+	}
+
+}
