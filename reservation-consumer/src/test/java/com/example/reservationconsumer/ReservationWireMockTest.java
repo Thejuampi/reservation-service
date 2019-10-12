1fa3bc714fe2c17a81029efd1e21afafbca08c9c
Juan Pablo Lescano <juanpablo.lescano@gmail.com>
2019-10-12 19:31:08 -0300

    reservation consumer app with contract. Fails because the field name is not mapped and should be reservationName instead

--- reservation-consumer/src/test/java/com/example/reservationconsumer/ReservationWireMockTest.java
+++ reservation-consumer/src/test/java/com/example/reservationconsumer/ReservationWireMockTest.java
@@ -1,48 +1,21 @@
 package com.example.reservationconsumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import lombok.SneakyThrows;
-import lombok.val;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.test.StepVerifier;
 
-import java.util.List;
-
 @ExtendWith(SpringExtension.class)
-@AutoConfigureWireMock(port = 8080)
-//@AutoConfigureJsonTesters gives an object mapper, which gives the validation stage for the obj mapper. Object mapper is used to get data in and out
-@AutoConfigureJsonTesters
+@AutoConfigureStubRunner(ids = "com.example:reservation-service:+:8080", stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 @Import({ReservationConsumerApplication.class, ReservationClient.class})
 class ReservationWireMockTest {
 
   @Autowired
   ReservationClient client;
-
-  @Autowired
-  ObjectMapper objectMapper;
-
-  @SneakyThrows
-  @BeforeEach
-  void setUp() {
-    val json = objectMapper.writeValueAsString(List.of(
-        new Reservation("1", "PABLO")
-    ));
-
-    WireMock.stubFor(WireMock.get("/reservations")
-        .willReturn(WireMock.aResponse()
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
-            .withBody(json)));
-  }
 
   @Test
   void testShouldClientReturn() {
